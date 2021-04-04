04b8a7eff46c630e3077c24d8e530b305c7c1302
daurnimator <quae@daurnimator.com>
2021-04-04 22:25:43 +1000

    Set up build.zig test step

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,9 @@
-const message = @import("message");
+const std = @import("std");
+const testing = std.testing;
+
+const channel = @import("./channel.zig");
+const message = @import("./message.zig");
+
+test {
+    testing.refAllDecls(@This());
+}
